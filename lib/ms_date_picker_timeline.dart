511a72576128019a9f88c7f471c662f064132d57
Jesus Manuel Vargas <jvargas@vente-privee.com>
2020-01-10 13:52:48 +0100

    fix

--- lib/ms_date_picker_timeline.dart
+++ lib/ms_date_picker_timeline.dart
@@ -98,7 +98,6 @@ class _MSDatePickerState extends State<MSDatePickerTimeline> {
     var numberOfDays = widget.endDate.difference(widget.startDate).inDays + 1;
     var rightLimit = numberOfDays - 5
     if (position > rightLimit) position = rightLimit;
-    print(rightDifference);
     print("position: " + position.toString());
     widget._scrollController.animateTo(position * 62.0,
           duration: new Duration(seconds: 1), curve: Curves.ease);
