614cf29dc85a69a855eb3328bc9d13e6b16a14a7
Jesus Manuel Vargas <jvargas@vente-privee.com>
2020-01-10 14:02:28 +0100

    testing a new value

--- lib/ms_date_picker_timeline.dart
+++ lib/ms_date_picker_timeline.dart
@@ -96,7 +96,7 @@ class _MSDatePickerState extends State<MSDatePickerTimeline> {
 
   void scrollToPosition(int position) {
     var numberOfDays = widget.endDate.difference(widget.startDate).inDays + 1;
-    var rightLimit = numberOfDays - 6;
+    var rightLimit = numberOfDays - 4;
     if (position > rightLimit) position = rightLimit;
     print("position: " + position.toString());
     widget._scrollController.animateTo(position * 62.0,
